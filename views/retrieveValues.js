4abd0fc6a7dd7546e97004c52df12608076475cd
FileStream <jpahman2003@gmail.com>
2018-05-11 12:51:53 -0500

    Create retrieveValues.js

---
+++ views/retrieveValues.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+return [document.getElementById("firstname").value+document.getElementById("lastname").value,[document.getElementsByName("likes").filter(x=>x.checked==true).map(x=x.id)]];
+}

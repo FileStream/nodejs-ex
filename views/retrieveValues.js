6c0568dff4496ca889d79fd5a1574845b9b35d5b
FileStream <jpahman2003@gmail.com>
2018-05-11 16:50:57 -0500

    Update retrieveValues.js

--- views/retrieveValues.js
+++ views/retrieveValues.js
@@ -1,3 +1,4 @@
-module.exports = function() {
+module.exports = { function() {
 return [document.getElementById("firstname").value+document.getElementById("lastname").value,[document.getElementsByName("likes").filter(x=>x.checked==true).map(x=x.id)]];
+}
 }

d3e5507f0fe36305af0b4d2fdf8c1340a71436d7
FileStream <jpahman2003@gmail.com>
2018-05-11 17:16:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -58,6 +58,19 @@ var initDb = function(callback) {
   });
 };
 
+app.get('/', function (req, res) {
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
+});
+
 app.get('/studentform', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
